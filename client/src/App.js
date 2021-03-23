c0aedbf845bd3c2c621b9944c8873fbb36d655e9
munozjuan <munoz.juan07@hotmail.com>
2021-03-22 21:52:07 -0400

    debugged

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,48 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Saved from "./pages/Saved";
-import "./App.css";
+import Saved from "./pages/saved";
+import Search from "./pages/search";
+import Nav from "./components/Nav";
+import { Container} from "./components/Grid";
+import { ToastContainer, Zoom} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+// import {ToastProvider, useToasts} from "react-toast-notifications"
 
+function App(){
 
-
-class App extends Component {
-
-
-  render() {
+  
     return (
-      <Router>
       <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/saved" component={Saved} />
-        </Switch>
+      
+        <Nav/>
+        <Router>
+          <Container>
+          {/* <ToastContainer/> */}
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            transition={Zoom}
+            hideProgressBar
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnVisibilityChange
+            draggable={false}
+            pauseOnHover
+          />
+            <Switch>
+              <Route exact path="/" component={Search} />
+              <Route path="/bookshelf" component={Saved} />
+              <Route exact path="/books/:id" component={null} />
+              <Route component={null} />
+            </Switch>
+          </Container>
+        </Router>
+      
       </div>
-      </Router>
     );
-  }
+  
 }
 
 export default App;
--- client/src/components/Grid/index.js
+++ client/src/components/Grid/index.js
@@ -25,4 +25,4 @@ export function Col({ size, children }) {
       {children}
     </div>
   );
-      }
+}

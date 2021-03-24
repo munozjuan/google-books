83ca29b7abdcbf8836d48b114cf2d2d0edecada0
munozjuan <munoz.juan07@hotmail.com>
2021-03-23 20:44:47 -0400

    working with react styles

--- client/src/pages/search/index.js
+++ client/src/pages/search/index.js
@@ -5,7 +5,7 @@ import AddBookBtn from "../../components/AddBookBtn";
 import { Row, Col } from "../../components/Grid";
 import { BookList, BookListItem } from "../../components/BookList";
 import EmptyList from "../../components/EmptyList";
-
+import { Button } from 'reactstrap';
 
 class Search extends Component {
   state = {
@@ -49,11 +49,10 @@ class Search extends Component {
         <Col size="md-12">
         <div>
           <input id="bookQ" className="form-control form-control-lg" autoComplete="off" type="text" name="query" onChange={this.handleInput} />
-          <button  type="submit" onClick={this.searchGBooks} >
+          <Button type="submit" variant="outline-dark" onClick={this.searchGBooks} >
             Search for Books
-          </button>
-                   
-
+          </Button>
+          
           {(this.state.books && this.state.books.length > 0) ? 
           <BookList>
           {this.state.books.map(book => {
